6fc33524a841f49743a28605b27c17b072cfa789
qitry <qitryt@sina.cn>
2026-06-06 15:41:25 +0800

    fix: restore missing R import in MainMenuItemViewHolder

--- app/src/main/java/com/termux/zerocore/config/mainmenu/view/viewholder/MainMenuItemViewHolder.java
+++ app/src/main/java/com/termux/zerocore/config/mainmenu/view/viewholder/MainMenuItemViewHolder.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.termux.R;
+
 public class MainMenuItemViewHolder extends RecyclerView.ViewHolder {
     public TextView mCodeTitle;
     public LinearLayout mItemlayout;
